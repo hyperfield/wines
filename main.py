e353dc4abc4c93852b326de3b4232c1667775b18
weird.structure <sequence@myrealm.ca>
2020-11-05 20:36:19 -0700

    Modified main.py for PEP8

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ import pandas
 import collections
 
 if __name__ == "__main__":
-	
 	env = Environment(
 	loader=FileSystemLoader('.'),
     	autoescape=select_autoescape(['html', 'xml'])
 	)
-	
 	template = env.get_template('template.html')
-	
-	excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', usecols=['Картинка', 'Категория', 'Название', 'Сорт', 'Цена', 'Акция']).to_dict(orient='records')
+	excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',
+									usecols=['Картинка', 'Категория', 'Название',
+									'Сорт', 'Цена', 'Акция']).to_dict(orient='records')
 	wines = collections.defaultdict(list)
 
 	for drink in excel_data_df:
 		wines[drink['Категория']].append(drink)
-	
-	wines = collections.OrderedDict(sorted(wines.items(), key=lambda t: t[0])) # Отсортировать словарь в алфавитном порядке по ключу 'Категория'
-		
+
+	wines = collections.OrderedDict(sorted(wines.items(),\
+		 							key=lambda t: t[0])) # Отсортировать словарь в алфавитном порядке по ключу 'Категория'
 	rendered_page = template.render(wines=wines)
 
 	with open('index.html', 'w', encoding="utf8") as file:
-    		file.write(rendered_page)
+		file.write(rendered_page)
 
 	server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 	server.serve_forever()
