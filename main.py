1d956a516f7c87f50b8a65959bc4e976921a0954
weird.structure <sequence@myrealm.ca>
2020-11-09 21:54:55 -0700

    Code synax improvements in main.py, template.html.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
+import sys
+import argparse
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('path', nargs='?', default='wine3.xlsx')
+     
+    return parser
 
 if __name__ == "__main__":
+	parser = createParser()
+	namespace = parser.parse_args (sys.argv[1:])
 	env = Environment(
 	loader=FileSystemLoader('.'),
     	autoescape=select_autoescape(['html', 'xml'])
 	)
 	template = env.get_template('template.html')
-	excel_dataframe = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',
+	excel_dataframe = pandas.read_excel(namespace.path, sheet_name='Лист1',
 									usecols=['Картинка', 'Категория', 'Название',
 									'Сорт', 'Цена', 'Акция']).to_dict(orient='records')
 	wines = collections.defaultdict(list)
@@ -20,7 +33,11 @@ if __name__ == "__main__":
 
 	wines = collections.OrderedDict(sorted(wines.items(),\
 		 							key=lambda t: t[0])) # Отсортировать словарь в алфавитном порядке по ключу 'Категория'
-	rendered_page = template.render(wines=wines)
+		 							
+	
+	rendered_page = template.render(
+			wines=wines,
+			age=datetime.datetime.now().year-1920)
 
 	with open('index.html', 'w', encoding="utf8") as file:
 		file.write(rendered_page)
